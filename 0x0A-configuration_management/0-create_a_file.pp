8d2d42b8b7eab8d6819268223fa15184887722eb
Jocseh07 <josephmkaranja07@gmail.com>
2024-04-19 12:37:59 +0300

    more

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file in /tmp
 
 file { '/tmp/school':
-  content => 'I love puppet',
-  mode    => '0744',
-  owner   => 'www-date',
-  group   => 'www-data',
+  content =>'I love puppet',
+  mode    =>'0744',
+  owner   =>'www-date',
+  group   =>'www-data',
 }
