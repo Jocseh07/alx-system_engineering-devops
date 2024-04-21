37b8e63f2555e2f23de5b07a60c2b43efa006e9a
Jocseh07 <josephmkaranja07@gmail.com>
2024-04-21 21:32:13 +0300

    more

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,16 @@
 # config with puppet
 
 file_line { 'No password':
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => '    PasswordAuthentication no'
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    line    => '    PasswordAuthentication no'.
+    replace => true,
+
 }
 
 file_line { 'IdentityFile'
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => '    IdentityFile ~/.ssh/school'
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    line    => '    IdentityFile ~/.ssh/school',
+    replace => true,
 }
