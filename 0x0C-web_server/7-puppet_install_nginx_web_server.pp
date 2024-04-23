0b9728b3679d8a5819d7e9c0f176377dd4de52d4
Jocseh07 <josephmkaranja07@gmail.com>
2024-04-24 01:03:26 +0300

    more

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,6 @@
+# Install nginx server
+
+exec { 'install':
+  provider => shell,
+  command  => "sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo ufw allow 'Nginx HTTP' ; echo 'Hello World!' >/var/www/html/index.nginx-debian.html ; CONFIG=\"server {\n    listen 80 default_server;\n	listen [::]:80 default_server;\n    root /var/www/html;\n	index index.html index.htm index.nginx-debian.html;\n    server_name _;\n    location / {\n		try_files \$uri \$uri/ =404;\n	}\n    if ( \$request_filename ~ redirect_me ) {\n        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent; \n    }\n}\" ; echo -e \"$CONFIG\" >/etc/nginx/sites-enabled/default ; service nginx restart "
+}
