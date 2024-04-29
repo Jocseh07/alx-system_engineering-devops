124607caadb34e2050f4b8e0647c94febcb92fc5
Jocseh07 <josephmkaranja07@gmail.com>
2024-04-29 15:45:36 +0300

    more

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,5 +2,9 @@
 
 exec { 'install':
   provider => shell,
-  command  => "sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo ufw allow 'Nginx HTTP' ; sudo echo 'Hello World!' >/var/www/html/index.nginx-debian.html ; sudo echo -e 'server {\n    listen 80 default_server;\n	  listen [::]:80 default_server;\n    root /var/www/html;\n	  index index.html index.htm index.nginx-debian.html;\n    server_name _;\n    location / {\n		try_files \$uri \$uri/ =404;\n	}\n    if ( \$request_filename ~ redirect_me ) {\n        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n    }\n}' >/etc/nginx/sites-enabled/default ; service nginx restart ; sudo sed -i \"s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/\" /etc/nginx/nginx.conf ; sudo service nginx restart"
+  command  => "sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo ufw allow 'Nginx HTTP' ; sudo echo 'Hello World!' >/var/www/html/index.nginx-debian.html ; sudo echo -e 'server {\n    listen 80 default_server;\n	  listen [::]:80 default_server;\n    root /var/www/html;\n	  index index.html index.htm index.nginx-debian.html;\n    server_name _;\n    location / {\n		try_files \$uri \$uri/ =404;\n	}\n    if ( \$request_filename ~ redirect_me ) {\n        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n    }\n}' >/etc/nginx/sites-enabled/default ; service nginx restart ;"
+}
+exec { 'add_header':
+  provider => shell,
+  command  => "sed -i \"s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/\" /etc/nginx/nginx.conf  service nginx restart ;"
 }
