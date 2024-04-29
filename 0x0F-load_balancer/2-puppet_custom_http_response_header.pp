89613d6cc05984931bbca57a41a07c36e911c19c
Jocseh07 <josephmkaranja07@gmail.com>
2024-04-29 15:56:01 +0300

    Revert "more"
    This reverts commit 7d0b448c47c1b9e175989a9bcfcba95f27fbdb16.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,6 +2,5 @@
 
 exec { 'add_header':
   provider => shell,
-  environment => ["HOSTNAME=${hostname}"]
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf ;  service nginx restart ;'
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo ufw allow "Nginx HTTP" ; sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf ;  service nginx restart ;'
 }
