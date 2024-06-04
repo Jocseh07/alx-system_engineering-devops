34883379d8f734baadfad60d956de2845f975446
Joseph Muturi <129220929+Jocseh07@users.noreply.github.com>
2024-06-04 12:34:00 +0300

    api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,12 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url, allow_redirects=False)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US)\
+        AppleWebKit/525.19 (KHTML, like Gecko) Chrome/1.0.154.53 Safari/525.19"
+    }
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False, params=params)
     if response.status_code != 200:
         return 0
     return response.json()["data"]["subscribers"]
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,12 @@ def top_ten(subreddit):
     """Return the number of subscribers for a subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/hot/.json"
     params = {"limit": 10}
-    response = requests.get(url, allow_redirects=False, params=params)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US)\
+        AppleWebKit/525.19 (KHTML, like Gecko) Chrome/1.0.154.53 Safari/525.19"
+    }
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False, params=params)
     if response.status_code != 200:
         print("None")
         return
